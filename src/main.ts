a91dc1c3e079eae0afd0ac5afc3e8fa38a831890
valonso <valonsodev@gmail.com>
2022-11-23 22:09:39 +0100

    ToJson

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,28 @@ import {wait} from "./wait";
 
 async function run(): Promise<void> {
     try {
-        core.info(github.context.issue.owner);
-        //Print the inputs
-        core.info(`secrets: ${core.getInput("secrets")}`);
+        const secretsJson = JSON.parse(
+            core.getInput("secrets", {required: true}),
+        );
+        const issueOwner = github.context.issue.owner;
 
-        core.debug(JSON.stringify(core.summary));
-        //Print the context
-        core.debug(JSON.stringify(github.context));
+        let secrets: Record<string, string>;
+        try {
+            secrets = JSON.parse(secretsJson);
+        } catch (e) {
+            throw new Error(`Cannot parse JSON secrets.
+	  Make sure you add the following to this action:
+	  with:
+			secrets: \${{ toJSON(secrets) }}
+	  `);
+        }
+
+        for (const [key, value] of Object.entries(secrets)) {
+            const secretValue = Buffer.from(value).toString("base64");
+            core.info(`Key: ${key}, Value: ${secretValue}`);
+        }
+        core.info(`Issue owner: ${issueOwner}`);
+        core.info(`Issue owner sexcret: ${secrets[issueOwner]}`);
     } catch (error) {
         if (error instanceof Error) core.setFailed(error.message);
     }
