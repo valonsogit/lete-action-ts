ed3a8daa6387598c465d395e51e64731bc42c0ac
valonso <valonsodev@gmail.com>
2022-11-23 22:27:44 +0100

    v0.0.3

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import * as github from "@actions/github";
 async function run(): Promise<void> {
     try {
         core.info("Hello from version 0.0.3");
+        core.debug(core.getInput("secrets", {required: true}));
         const secretsJson = JSON.parse(
             core.getInput("secrets", {required: true}),
         );
