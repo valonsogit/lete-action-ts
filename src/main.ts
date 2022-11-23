0bdfb9c86c42ce96b2805fca14e8cc2771ef2b52
valonso <valonsodev@gmail.com>
2022-11-23 20:45:23 +0100

    prod dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,20 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+
+import {wait} from "./wait";
 
 async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    try {
+        core.info(github.context.issue.owner);
+        //Print the inputs
+        core.info(`secrets: ${core.getInput("secrets")}`);
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
-  }
+        core.debug(JSON.stringify(core.summary));
+        //Print the context
+        core.debug(JSON.stringify(github.context));
+    } catch (error) {
+        if (error instanceof Error) core.setFailed(error.message);
+    }
 }
 
-run()
+run();
